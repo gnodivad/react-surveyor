530a369682ac0eb19c57b95ec5a858d60d81d9ba
DavidODW <dodwei.dev@gmail.com>
2018-04-27 14:45:42 +0800

    Add cookie-session and logic

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -15,10 +25,13 @@ passport.use(
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }).then(existingUser => {
                 if (existingUser) {
+                    done(null, existingUser);
                 } else {
                     new User({
                         googleId: profile.id
-                    }).save();
+                    })
+                        .save()
+                        .then(user => done(null, user));
                 }
             });
         }
