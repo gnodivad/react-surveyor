fdbcddbb09fa780be69127a1e5e2af6ffda7bb6f
DavidODW <dodwei.dev@gmail.com>
2018-07-17 17:16:02 +0800

    Connect header with state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return "Still deciding";
+            case false:
+                return "im loggedout";
+            default:
+                return "im logged in";
+        }
+    }
+
     render() {
         return (
             <nav>
@@ -10,7 +22,7 @@ class Header extends Component {
                     </a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li>
-                            <a>Login With Google</a>
+                            <a>{this.renderContent()}</a>
                         </li>
                     </ul>
                 </div>
@@ -19,4 +31,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
