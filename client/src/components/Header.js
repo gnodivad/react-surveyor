9d6e4067582abf058f378e9946c6140c7f750f44
DavidODW <dodwei.dev@gmail.com>
2018-07-17 14:56:52 +0800

    Import axios redux-thunk

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
             <nav>
                 <div className="nav-wrapper">
                     <a href="#" className="brand-logo">
-                        Logo
+                        Surveyor
                     </a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li>
