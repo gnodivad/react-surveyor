bfc5aaea3cf42d21e3f6158388ad22ba4b95b32d
DavidODW <dodwei.dev@gmail.com>
2018-07-19 17:23:55 +0800

    Add link tag to website logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
     renderContent() {
@@ -25,9 +26,9 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="#" className="brand-logo">
+                    <Link to={this.props.auth ? "/survey" : "/"} className="brand-logo">
                         Surveyor
-                    </a>
+                    </Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li>
                             <a>{this.renderContent()}</a>
