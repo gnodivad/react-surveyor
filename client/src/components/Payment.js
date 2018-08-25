31932f83f9e4c03046d16576a4bc0babb91b797c
David Ong <dodwei.dev@gmail.com>
2018-08-25 08:42:16 +0800

    Customize add credits button

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -5,10 +5,14 @@ class Payment extends Component {
     render() {
         return (
             <StripeCheckout
+                name="React-Surveyor"
+                description="$5 for 5 email credits"
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">Add Credits</button>
+            </StripeCheckout>
         );
     }
 }
