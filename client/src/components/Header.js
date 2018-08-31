17d923ffc212164a53281d88ffd19699022eb139
David Ong <dodwei.dev@gmail.com>
2018-08-31 17:02:02 +0800

    Install redux-form

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -33,7 +33,7 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <Link to={this.props.auth ? "/survey" : "/"} className="brand-logo">
+                    <Link to={this.props.auth ? "/surveys" : "/"} className="brand-logo">
                         Surveyor
                     </Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
