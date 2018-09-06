341001e994517af93c6b32f0933b1b9be912255c
David Ong <dodwei.dev@gmail.com>
2018-09-06 14:22:49 +0800

    Extract survey form fields to another file for reuse

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SurveyFormReview = ({ onCancel }) => {
+const FIELDS = [
+    { label: "Survey Title", name: "title" },
+    { label: "Subject Line", name: "subject" },
+    { label: "Email Body", name: "body" },
+    { label: "Recipient List", name: "emails" }
+];
+
+const SurveyFormReview = ({ onCancel, formValues }) => {
     return (
         <div>
             <h5>Please confirm your entries</h5>
+            <div>
+                <div>
+                    <label>Survey Title</label>
+                    <div>{formValues.title}</div>
+                </div>
+            </div>
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
